3d6dc5d00919c8c783495824b222b70b750059d6
h4r4dream <emabb4ng3l@gmail.com>
2024-04-05 22:38:22 +0200

    feat: adding the routing options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { ContactComponent } from './contact/contact.component';
+
 
 @Component({
   selector: 'nfc-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nfc-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
