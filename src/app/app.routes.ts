0fcb4e2eb1c6ae523286da07ab12988f2bf0c4e0
h4r4dream <emabb4ng3l@gmail.com>
2024-04-05 23:11:57 +0200

    feat: adding a 404 error route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'contact', component: ContactComponent },
+    { path: '**', component: NotfoundComponent },
 ];
---
+++ src/app/notfound/notfound.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nfc-notfound',
+  standalone: true,
+  imports: [],
+  templateUrl: './notfound.component.html',
+  styleUrl: './notfound.component.scss'
+})
+export class NotfoundComponent {
+
+}
